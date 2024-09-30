2183a49a0c14f7e8df53af4075a6b248c36dec54
ksmilga <94834460+ksmilga@users.noreply.github.com>
2024-09-30 12:10:19 +0300

    fix: add semicolon for php autocomplete

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,22 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    config = function()
+        local npairs = require('nvim-autopairs')
+        local Rule = require('nvim-autopairs.rule')
+
+        npairs.setup({
+            -- your setup options here
+        })
+
+        -- Custom rule for PHP method autocomplete with semicolon
+        npairs.add_rules({
+            Rule("%)$", ";", "php")
+                :use_regex(true)
+                :replace_endpair(function(opts)
+                    return opts.prev_char:match("%)$") and ";" or ""
+                end)
+                :set_end_pair_length(0)
+        })
+    end
 }
