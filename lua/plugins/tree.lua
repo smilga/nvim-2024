60b6760a123faed90abee0f40a9316045932505d
ksmilga <94834460+ksmilga@users.noreply.github.com>
2024-10-23 18:13:14 +0300

    fix: treeview follow cursor

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,15 +1,20 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			git = {
-				enable = true,
-				ignore = false,
-				timeout = 500,
-			},
-		})
-	end,
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup({
+            update_focused_file = {
+                enable = true,
+                update_root = false,
+            },
+            git = {
+                enable = true,
+                ignore = false,
+                timeout = 500,
+            },
+        })
+    end,
 }
+
