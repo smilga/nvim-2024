602290d6251c7b3fd9b243f6320579a470919071
ksmilga <94834460+ksmilga@users.noreply.github.com>
2024-10-25 15:12:40 +0300

    fix: fix autopari single quote in double quote

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,29 +2,42 @@ return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true,
-    -- config = function()
-    --     local npairs = require('nvim-autopairs')
-    --     local Rule = require('nvim-autopairs.rule')
-    --
-    --     npairs.setup({
-    --         -- your setup options here
-    --     })
-    --
-    --     -- Custom rule for PHP method autocomplete with semicolon
-    --     npairs.add_rules({
-    --         Rule("%)$", ";", "php")
-    --             :use_regex(true)
-    --             :replace_endpair(function(opts)
-    --                 local line = opts.line
-    --                 local col = opts.col
-    --                 local char_after = line:sub(col + 1, col + 1)
-    --                 if opts.prev_char:match("%)$") and char_after:match("^%s*$") then
-    --                     return ";"
-    --                 else
-    --                     return ""
-    --                 end
-    --             end)
-    --             :set_end_pair_length(0)
-    --     })
-    -- end
+    config = function()
+        local npairs = require('nvim-autopairs')
+        local Rule = require('nvim-autopairs.rule')
+
+        npairs.setup({
+            check_ts = true
+        })
+        npairs.add_rules({
+            Rule("'", "'")
+                :with_pair(function(opts)
+                    -- Check if we're inside double quotes
+                    local line = opts.line
+                    local col = opts.col
+
+                    -- Count quotes before cursor to determine if we're inside string
+                    local count = 0
+                    for i = 1, col - 1 do
+                        if line:sub(i, i) == '"' and line:sub(i - 1, i - 1) ~= '\\' then
+                            count = count + 1
+                        end
+                    end
+
+                    -- If count is odd, we're inside a string
+                    -- Also check that next char isn't alphanumeric
+                    local next_char = line:sub(col, col)
+                    return count % 2 == 1 and not next_char:match("%w")
+                end)
+                :with_move(function(opts)
+                    return opts.char == "'"
+                end),
+
+            Rule('"', '"')
+                :with_pair(function(opts)
+                    local next_char = opts.line:sub(opts.col, opts.col)
+                    return not next_char:match("%w")
+                end)
+        })
+    end
 }
