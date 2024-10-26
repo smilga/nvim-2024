42aac4ba7d858210a55a580219125e89b2cf5937
ksmilga <94834460+ksmilga@users.noreply.github.com>
2024-10-26 13:04:21 +0300

    feat: rename html tags plugin

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,28 @@
+return {
+    'windwp/nvim-ts-autotag',
+    lazy = false,
+    config = function()
+        require('nvim-ts-autotag').setup({
+            debug = true,
+            opts = {
+                -- Defaults
+                enable_close = true,          -- Auto close tags
+                enable_rename = true,         -- Auto rename pairs of tags
+                enable_close_on_slash = false -- Auto close on trailing </
+            },
+            -- Also override individual filetype configs, these take priority.
+            -- Empty by default, useful if one of the "opts" global settings
+            -- doesn't work well in a specific filetype
+            per_filetype = {
+                ["html"] = {
+                    enable_close = false
+                },
+                ["vue"] = {
+                    enable_close = true,         -- Auto close tags
+                    enable_rename = true,        -- Auto rename pairs of tags
+                    enable_close_on_slash = true -- Auto close on trailing </
+                },
+            }
+        })
+    end
+}
