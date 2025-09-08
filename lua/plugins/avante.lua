02618457cae60601365f307ae83179c8fef0a2b9
Kaspars <kaspars.smilga@datamed.lv>
2025-09-08 14:05:21 +0300

    fix: update avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,50 +1,53 @@
 return {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- set this if you want to always pull the latest change
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",      -- for providers='copilot'
-        {
-            -- support for image pasting
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    -- required for Windows users
-                    use_absolute_path = true,
-                },
-            },
-        },
-        {
-            -- Make sure to set this up properly if you have lazy=true
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { "markdown", "Avante" },
-            },
-            ft = { "markdown", "Avante" },
-        },
-    },
-    config = function()
-        require('img-clip').setup()
-        require('render-markdown').setup()
-        require('avante_lib').load()
-        require('avante').setup({
-            auto_suggestions_provider = 'copilot',
-            windows = {
-                position = 'right'
-            },
-        })
-    end,
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- set this if you want to always pull the latest change
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					-- required for Windows users
+					use_absolute_path = true,
+				},
+			},
+		},
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+	config = function()
+		require("img-clip").setup()
+		require("render-markdown").setup()
+		require("avante_lib").load()
+		require("avante").setup({
+			auto_suggestions_provider = "copilot",
+			windows = {
+				position = "right",
+			},
+			behavior = {
+				auto_suggestions = false,
+			},
+		})
+	end,
 }
