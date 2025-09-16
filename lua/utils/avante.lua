82b1feec0861b2da68602e1741f8f4c63440a4d7
Kaspars <kaspars.smilga@datamed.lv>
2025-09-16 10:25:27 +0300

    fix: remove unecessary config

--- lua/utils/avante.lua
+++ lua/utils/avante.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-function M.setup()
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = { "Avante", "AvanteSelectedFiles", "AvanteInput" },
-	})
-end
+function M.setup() end
 
 return M
