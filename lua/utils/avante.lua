dadeaaa957897bbb0be2d23b4d0d73639604c1bf
Kaspars <kaspars.smilga@datamed.lv>
2025-09-16 10:22:48 +0300

    fix: update more keys

--- lua/utils/avante.lua
+++ lua/utils/avante.lua
@@ -1,12 +1,9 @@
 local M = {}
 
 function M.setup()
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "Avante", "AvanteSelectedFiles", "AvanteInput" },
-        callback = function()
-            vim.keymap.set("n", "<Esc>", ":close<CR>", { buffer = true })
-        end,
-    })
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = { "Avante", "AvanteSelectedFiles", "AvanteInput" },
+	})
 end
 
 return M
